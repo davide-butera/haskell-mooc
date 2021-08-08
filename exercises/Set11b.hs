06094e3279ae3261fc27a77c69cd8036b6a7ffcc
Davide Butera <42903838+davide-butera@users.noreply.github.com>
2021-08-08 03:37:40 +0200

    Added Set11b

--- exercises/Set11b.hs
+++ exercises/Set11b.hs
@@ -1,11 +1,11 @@
 module Set11b where
 
-import Control.Monad
-import Data.List
-import Data.IORef
-import System.IO
+import           Control.Monad
+import           Data.IORef
+import           Data.List
+import           System.IO
 
-import Mooc.Todo
+import           Mooc.Todo
 
 
 ------------------------------------------------------------------------------
@@ -20,7 +20,7 @@ import Mooc.Todo
 --   "xfoobarquux"
 
 appendAll :: IORef String -> [String] -> IO ()
-appendAll = todo
+appendAll s xs = modifyIORef s (++concat xs)
 
 ------------------------------------------------------------------------------
 -- Ex 2: Given two IORefs, swap the values stored in them.
@@ -35,7 +35,10 @@ appendAll = todo
 --   "x"
 
 swapIORefs :: IORef a -> IORef a -> IO ()
-swapIORefs = todo
+swapIORefs x y = do temp <- readIORef x
+                    y'   <- readIORef y
+                    writeIORef x y'
+                    writeIORef y temp
 
 ------------------------------------------------------------------------------
 -- Ex 3: sometimes one bumps into IO operations that return IO
@@ -61,8 +64,9 @@ swapIORefs = todo
 --        replicateM l getLine
 
 doubleCall :: IO (IO a) -> IO a
-doubleCall op = todo
-
+doubleCall op = do join op
+             -- do res  <- op
+             --    res
 ------------------------------------------------------------------------------
 -- Ex 4: implement the analogue of function composition (the (.)
 -- operator) for IO operations. That is, take an operation op1 of type
@@ -80,7 +84,8 @@ doubleCall op = todo
 --   3. return the result (of type b)
 
 compose :: (a -> IO b) -> (c -> IO a) -> c -> IO b
-compose op1 op2 c = todo
+compose op1 op2 c = do a <- op2 c
+                       op1 a
 
 ------------------------------------------------------------------------------
 -- Ex 5: Reading lines from a file. The module System.IO defines
@@ -110,7 +115,8 @@ compose op1 op2 c = todo
 --   ["module Set11b where","","import Control.Monad"]
 
 hFetchLines :: Handle -> IO [String]
-hFetchLines = todo
+hFetchLines h = do contents <- hGetContents h
+                   return $ lines contents
 
 ------------------------------------------------------------------------------
 -- Ex 6: Given a Handle and a list of line indexes, produce the lines
@@ -123,7 +129,34 @@ hFetchLines = todo
 -- handle.
 
 hSelectLines :: Handle -> [Int] -> IO [String]
-hSelectLines h nums = todo
+hSelectLines h nums = do
+    lines <- hFetchLines h
+    return [j | (i,j) <- zip [1..] lines, i `elem` nums]
+
+-- hSelectLines' :: Handle -> [Int] -> Int -> [String] -> IO [String]
+-- hSelectLines' h nums idx selectedLines = do
+--     eof <- hIsEOF h
+--     if eof then return selectedLines
+--     else do
+--         l <- hGetLine h
+--         if   idx `elem` nums
+--         then hSelectLines' h nums (idx+1) (selectedLines++[l])
+--         else hSelectLines' h nums (idx+1) selectedLines
+
+
+
+-- hSelectLines :: Handle -> [Int] -> IO [String]
+-- hSelectLines h nums = hSelectLines' h nums 1 []
+
+-- hSelectLines' :: Handle -> [Int] -> Int -> [String] -> IO [String]
+-- hSelectLines' h nums idx selectedLines = do
+--     eof <- hIsEOF h
+--     if eof then return selectedLines
+--     else do
+--         l <- hGetLine h
+--         if   idx `elem` nums
+--         then hSelectLines' h nums (idx+1) (selectedLines++[l])
+--         else hSelectLines' h nums (idx+1) selectedLines
 
 ------------------------------------------------------------------------------
 -- Ex 7: In this exercise we see how a program can be split into a
@@ -164,4 +197,10 @@ counter ("print",n) = (True,show n,n)
 counter ("quit",n)  = (False,"bye bye",n)
 
 interact' :: ((String,st) -> (Bool,String,st)) -> st -> IO st
-interact' f state = todo
+interact' f state = do
+    line <- getLine
+    let (bool, string, newstate) = f (line, state)
+    putStrLn string
+    if bool then
+        interact' f newstate
+    else return newstate
