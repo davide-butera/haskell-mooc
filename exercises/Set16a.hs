1a45b7d8c170d84e919feda7f57b7b4cfc62f991
Davide Butera <42903838+davide-butera@users.noreply.github.com>
2021-08-24 18:58:21 +0200

    Added solutions to 16a

--- exercises/Set16a.hs
+++ exercises/Set16a.hs
@@ -1,9 +1,9 @@
 module Set16a where
 
-import Mooc.Todo
-import Test.QuickCheck
+import           Mooc.Todo
+import           Test.QuickCheck
 
-import Data.List
+import           Data.List
 
 ------------------------------------------------------------------------------
 -- Ex 1: Write a Property that checks that a given list is sorted (in
@@ -18,7 +18,7 @@ import Data.List
 --  +++ OK, passed 1 test.
 
 isSorted :: (Show a, Ord a) => [a] -> Property
-isSorted = todo
+isSorted xs = sort xs === xs
 
 ------------------------------------------------------------------------------
 -- Ex 2: In this and the following exercises, we'll build a suite of
@@ -50,12 +50,12 @@ isSorted = todo
 --  +++ OK, passed 1 test.
 
 sumIsLength :: Show a => [a] -> [(a,Int)] -> Property
-sumIsLength input output = todo
+sumIsLength input output = length input === sum (map snd output)
 
 -- This is a function that passes the sumIsLength test but is wrong
 freq1 :: Eq a => [a] -> [(a,Int)]
-freq1 [] = []
-freq1 [x] = [(x,1)]
+freq1 []       = []
+freq1 [x]      = [(x,1)]
 freq1 (x:y:xs) = [(x,1),(y,length xs + 1)]
 
 ------------------------------------------------------------------------------
@@ -79,11 +79,11 @@ freq1 (x:y:xs) = [(x,1),(y,length xs + 1)]
 --  +++ OK, passed 100 tests.
 
 inputInOutput :: (Show a, Eq a) => [a] -> [(a,Int)] -> Property
-inputInOutput input output = todo
+inputInOutput input output = forAll (elements input) (\x -> x `elem` map fst output)
 
 -- This function passes both the sumIsLength and inputInOutput tests
 freq2 :: Eq a => [a] -> [(a,Int)]
-freq2 xs = map (\x -> (x,1)) xs
+freq2 = map (\x -> (x,1))
 
 ------------------------------------------------------------------------------
 -- Ex 4: Implement a Property that takes a pair (x,n) from the
@@ -110,11 +110,12 @@ freq2 xs = map (\x -> (x,1)) xs
 --  +++ OK, passed 100 tests.
 
 outputInInput :: (Show a, Eq a) => [a] -> [(a,Int)] -> Property
-outputInInput input output = todo
+outputInInput input output = forAll (elements output) (\(x,n) -> count x input == n)
+  where count x = length . filter (x==)
 
 -- This function passes the outputInInput test but not the others
 freq3 :: Eq a => [a] -> [(a,Int)]
-freq3 [] = []
+freq3 []     = []
 freq3 (x:xs) = [(x,1 + length (filter (==x) xs))]
 
 ------------------------------------------------------------------------------
@@ -139,7 +140,10 @@ freq3 (x:xs) = [(x,1 + length (filter (==x) xs))]
 --  +++ OK, passed 100 tests.
 
 frequenciesProp :: ([Char] -> [(Char,Int)]) -> NonEmptyList Char -> Property
-frequenciesProp freq input = todo
+frequenciesProp freq input =
+  conjoin [inputInOutput input' output, inputInOutput input' output, outputInInput input' output]
+      where input' = getNonEmpty input
+            output = freq input'
 
 frequencies :: Eq a => [a] -> [(a,Int)]
 frequencies [] = []
@@ -170,7 +174,9 @@ frequencies (x:ys) = (x, length xs) : frequencies others
 --  [2,4,10]
 
 genList :: Gen [Int]
-genList = todo
+genList = do l <- choose (3,5)
+             sort <$> vectorOf l (choose (0,10))
+
 
 ------------------------------------------------------------------------------
 -- Ex 7: Here are the datatypes Arg and Expression from Set 15. Write
@@ -208,7 +214,25 @@ data Expression = Plus Arg Arg | Minus Arg Arg
   deriving (Show, Eq)
 
 instance Arbitrary Arg where
-  arbitrary = todo
+  arbitrary = oneof [genNumber, genVariable]
+    where genNumber   = Number   <$> choose (1,10)
+          genVariable = Variable <$> elements "abcxyz"
 
 instance Arbitrary Expression where
-  arbitrary = todo
+  arbitrary = elements [Plus, Minus] <*> arbitrary <*> arbitrary
+
+-- instance Arbitrary Arg where
+--   arbitrary = oneof [genNumber, genVariable]
+
+-- genNumber :: Gen Arg
+-- genNumber = do l <- choose (1, 10)
+--                pure $ Number l
+
+-- genVariable :: Gen Arg
+-- genVariable = do v <- elements "abcxyz"
+--                  pure $ Variable v
+
+-- instance Arbitrary Expression where
+--   arbitrary = elements [Plus, Minus]         <*>
+--               oneof [genVariable, genNumber] <*>
+--               oneof [genVariable, genNumber]
